dd8cf4857edc5fbb7f5055f1b15df22d1806609a
soelapyae-htun <e1045754@u.nus.edu>
2022-10-29 09:06:15 +0800

    nothing

--- main.js
+++ main.js
@@ -1,13 +1,29 @@
-
-//Import Package
+//Import packages
 const express = require('express')
 const morgan = require('morgan')
 
+//Configure out port
+//Check if it is set in the invironment variable, if not then we default ot 3000
+const port = process.env.PORT || 3000
 
-//configure out port
-//check if it is set in the enviroment variable , if not then we default to 3000
-const port = process.env.port || 3000
+//Create an instance of express application
+const app =express()
 
+//Log all request
+app.use(morgan('common'))
 
-console.info(`port = ${port}`) 
-
+//GET /
+app.get('/',(req,resp)=>{
+    //what is the media type
+    resp.type('text/html')
+    //set the status code
+    resp.status(200)
+    //send the response
+    resp.send(`<h1>The current time is ${new Date()}`)
+})
+// Serve static resource
+//app.use("/assets", express.static(__dirname + "/static"))
+//Start our wev application
+app.listen(port, () => {
+    console.info(`Starting application on port ${port} at ${new Date()}`)
+})
